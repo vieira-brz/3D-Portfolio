cba53574cc02378f5946eee6f490318aa088703d
vieira-brz <vieirabraz2003@gmail.com>
2024-04-23 10:16:14 -0300

    feat: All 3D Scenes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,70 @@
+import { Canvas } from "@react-three/fiber"
+import { Suspense, useState } from "react"
+import Loader from "../components/Loader"
+import { Island } from "../models/Island"
+import Sky from "../models/Sky"
+import Bird from "../models/Bird"
+import Plane from "../models/Plane"
+
+{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+  POPUP
+</div> */}
+
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [isRotating, setIsRotating] = useState(false)
+
+    // Adjusting Island for Screen Size
+    const adjustIslandForScreenSize = () => {
+        let screenScale = null
+        let screenPosition = [0, -6.5, -43]
+        let rotation = [0.1, 4.7, 0]
+
+        if (window.innerWidth < 768) {
+            screenScale = [0.9, 0.9, 0.9]
+            screenPosition = [0, -6.5, -43.4]
+        } else {
+            screenScale = [1, 1, 1]
+            screenPosition = [0, -6.5, -43.4]
+        }
+
+        return [screenScale, screenPosition, rotation]
+    }
+    const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize()
+
+    // Adjusting the Plane for Screen Size
+    const adjustPlaneForScreenSize = () => {
+        let screenScale, screenPosition
+
+        // If screen width is less than 768px, adjust the scale and position
+        if (window.innerWidth < 768) {
+            screenScale = [1.5, 1.5, 1.5]
+            screenPosition = [0, -1.5, 0]
+        } else {
+            screenScale = [3, 3, 3]
+            screenPosition = [0, -4, -4]
+        }
+
+        return [screenScale, screenPosition]
+    }
+    const [planeScale, planePosition] = adjustPlaneForScreenSize()
+
+    return (
+        <section className="w-full h-screen relative">
+            <Canvas className={`w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"}`} camera={{ near: 0.1, far: 1000 }}>
+                <Suspense fallback={<Loader />}>
+                    <directionalLight position={[1, 1, 1]} intensity={2} />
+                    <ambientLight intensity={0.5} />
+                    <hemisphereLight skyColor='#b1e1ff' groundColor={'#000000'} intensity={1} />
+
+                    <Bird />
+                    <Sky />
+                    <Island position={islandPosition} scale={islandScale} rotation={islandRotation} isRotating={isRotating} setIsRotating={isRotating} />
+                    <Plane planeScale={planeScale} planePosition={planePosition} isRotating={isRotating} rotation={[0, 20, 0]} />
+                </Suspense>
+            </Canvas>
+        </section>
+    )
 }
 
 export default Home
